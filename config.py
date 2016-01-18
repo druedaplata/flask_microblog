a1f508db1bb263381e39440741fdb7426f5b83dc
Diego Rueda <sandiego206@gmail.com>
2016-01-18 10:58:50 -0500

    Up to chapter 10 - Pagination and text search

--- config.py
+++ config.py
@@ -9,8 +9,31 @@ OPENID_PROVIDERS = [
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
 ]
 
+
+# Database settings
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# Mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = 'sandiego206'
+MAIL_PASSWORD = 1095804002
+
+# Administrator list
+ADMINS = ['sandiego206@gmail.com']
+
+# Pagination
+POSTS_PER_PAGE = 3
+
+# Full text search
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+MAX_SEARCH_RESULTS = 50
+
+
+
+
+
